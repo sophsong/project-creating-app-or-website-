e14398fd972e7aa990da79c5b31ca5543a2b647e
sophsong <35240775+sophsong@users.noreply.github.com>
2018-09-19 14:35:27 -0400

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,8 @@
+// var announcement= "Your profile will close in 10 minutes, if not completed.";
+//
+// alert(announcement);
+
+var signUp = document.getElementById("details");
+
+signUp.addEventListener( "click", function() {
+  document.getElementById("details").innerHTML= "Thank you for signing up for fishBowl! Now, your next step is to Get Started!"; })
